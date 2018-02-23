749f2d2f9d3db66aecc1105920af0c4d60e4411c
anderMatt <matt10.anderson@gmail.com>
2018-02-23 13:07:07 -0500

    Required fields: Last Name, First Name, Street Number, Name, City, State, Zip

--- UI/src/app/g28-form/part-1/part-1.component.ts
+++ UI/src/app/g28-form/part-1/part-1.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+
 import { FormFieldLOVService } from "../../shared/FormFieldLOV.service";
+
 
 @Component({
   selector: 'app-part-1',
@@ -9,10 +12,24 @@ import { FormFieldLOVService } from "../../shared/FormFieldLOV.service";
 export class Part1Component implements OnInit {
   // TODO: have a form model.
   state: string = "";
+  attorneyInfoForm: FormGroup;
 
-  constructor(public lovService: FormFieldLOVService) { }
+  constructor(public lovService: FormFieldLOVService, private fb: FormBuilder) {
+    this.createForm();
+  }
 
   ngOnInit() {
+  }
+
+  createForm(): void {
+    this.attorneyInfoForm = this.fb.group({
+      lastName: ["", Validators.required],
+      firstName: ["", Validators.required],
+      streetNumber: ["", Validators.required],
+      city: ["", Validators.required],
+      state: ["", Validators.required],
+      zip: ["", Validators.required]
+    });
   }
 
 }
